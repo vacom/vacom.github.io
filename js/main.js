7f26ec43addaf5ddef20513c500dab1a01cd78cf
Vitor Amaral <va.works@hotmail.com>
2016-04-24 18:57:03 +0100

    js update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
+<<<<<<< HEAD
+jQuery(document).ready(function(){
+	if( $('.cd-stretchy-nav').length > 0 ) {
+		var stretchyNavs = $('.cd-stretchy-nav');
+		
+		stretchyNavs.each(function(){
+			var stretchyNav = $(this),
+				stretchyNavTrigger = stretchyNav.find('.cd-nav-trigger');
+			
+			stretchyNavTrigger.on('click', function(event){
+				event.preventDefault();
+				stretchyNav.toggleClass('nav-is-visible');
+			});
+		});
+
+		$(document).on('click', function(event){
+			( !$(event.target).is('.cd-nav-trigger') && !$(event.target).is('.cd-nav-trigger span') ) && stretchyNavs.removeClass('nav-is-visible');
+		});
+	}
+
+
+});
+
+
+
+
+=======
 jQuery(document).ready(function(){
 	if( $('.cd-stretchy-nav').length > 0 ) {
 		var stretchyNavs = $('.cd-stretchy-nav');
@@ -18,3 +45,4 @@ jQuery(document).ready(function(){
 	}
 
 });
+>>>>>>> master
